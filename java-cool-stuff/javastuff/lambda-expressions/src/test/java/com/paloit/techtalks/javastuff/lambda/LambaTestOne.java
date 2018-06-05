23e066b457dec323bb25aa0ca6e9745f76d380ec
Noe Max Montiel Hernandez <nmontielh@Psycho-Punk-2.local>
2018-06-05 09:48:38 -0500

    Se agrega la primera version delas pruebas del codigo par alas tech talks

---
+++ java-cool-stuff/javastuff/lambda-expressions/src/test/java/com/paloit/techtalks/javastuff/lambda/LambaTestOne.java
@@ -0,0 +1,13 @@
+package com.paloit.techtalks.javastuff.lambda;
+
+import org.testng.annotations.Test;
+
+/**
+ * Pruebas unitarias para programacion lamnda
+ * @author nmontielh
+ *
+ */
+@Test
+public class LambaTestOne {
+
+}
